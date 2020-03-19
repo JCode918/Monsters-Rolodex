eec693ef140ea296fe671d6567d3325a0c856aa9
JCode918 <Willard.Joseph26@gmail.com>
2020-03-18 23:19:56 -0400

    Added Stylesheets, changed to props, etc.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list-component';
 
 class App extends Component {
   constructor() {
     super();
+
 
     this.state = { monsters: [] };
   }
 
   /*Life Cycle Method from class: fetches from the url, changes the response to json format, and set the array of users which was retrieved from the url to the monsters array in the this.state above */
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({monsters: users}));
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({ monsters: users }));
   }
 
   render() {
     return (
+
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
-        }
+        <CardList monsters = {this.state.monsters}>
+          
+        </CardList>
+
       </div>
     );
   }
