01e0bf8f2515dd229c9527d0d03f80728a171100
JCode918 <Willard.Joseph26@gmail.com>
2020-03-20 10:25:53 -0400

    Final Step Before Deployment to Static Browser

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { CardList } from './components/card-list/card-list-component';
+import { SearchBox } from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor() {
@@ -12,6 +13,11 @@ class App extends Component {
       monsters: [],
       searchField: ''
     };
+
+    /*
+    *** This binding is no longer needed as I am using an arrow function below. Which automatically binds.
+    this.handleChange = this.handleChange.bind(this);
+    */
   }
 
   /*Life Cycle Method from class: fetches from the url, changes the response to json format, and set the array of users which was retrieved from the url to the monsters array in the this.state above */
@@ -19,18 +25,23 @@ class App extends Component {
     fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
-    
+
     const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
     // Tried to Add the Ability to Search Emails in the Search Field
-   // const filteredEmails = monsters.filter(monster => monster.email.toLowerCase().includes(searchField.toLowerCase()));
+    //const filteredEmails = monsters.filter(monster => monster.email.toLowerCase().includes(emailField.toLowerCase()));
     return (
       <div className="App">
-        <input type='search' placeholder='Monster Search' onChange={e => this.setState({ searchField: e.target.value })} />
-        <CardList monsters={filteredMonsters}/>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder='Monster Search' handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
