44a00792e11c747455ca2a087ad8bcf37c252995
JCode918 <Willard.Joseph26@gmail.com>
2020-03-31 00:16:00 -0400

    Updates

---
+++ precache-manifest.bad57d2b645eb653a9d0feee11fc4b92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c23889addf66de310201b2c68caeb63",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "84668eda1024dfbfd6b7",
+    "url": "/Monsters-Rolodex/static/css/main.57a4e881.chunk.css"
+  },
+  {
+    "revision": "ad8534d43364924f4cac",
+    "url": "/Monsters-Rolodex/static/js/2.bedd2c39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.bedd2c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84668eda1024dfbfd6b7",
+    "url": "/Monsters-Rolodex/static/js/main.44a58216.chunk.js"
+  },
+  {
+    "revision": "a61ad0e651ae2bf2e68e",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.2fa99291.js"
+  }
+]);
